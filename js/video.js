6cfcdcc55dab31a23c3f3377496b0e47d28e4109
sadhin28 <taosifsadhin113@gmail.com>
2025-04-20 01:46:38 +0600

    fetch data from link

---
+++ js/video.js
@@ -0,0 +1,14 @@
+const loadCatagories = ()=>{
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+    .then(res=>res.json())
+    .then((data)=>displayCatagories(data.categories
+    ))
+   
+    .catch((error)=> console.log(error))
+    
+}
+const displayCatagories = (data)=>{
+    console.log(data)
+}
+
+loadCatagories()
