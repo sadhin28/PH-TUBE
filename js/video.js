3cc3d9ea31ba048a924853bd3a8c97cd325ee395
sadhin28 <taosifsadhin113@gmail.com>
2025-04-20 04:17:52 +0600

    add card and varify batch

--- js/video.js
+++ js/video.js
@@ -1,37 +1,54 @@
-const LoadVideos = ()=>{
+const LoadVideos = () => {
     fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
-    .then(res=>res.json())
-    .then((data)=>displayVideos(data.videos
-    ))
-   
-    .catch((error)=> console.log(error))
+        .then(res => res.json())
+        .then((data) => displayVideos(data.videos
+        ))
+
+        .catch((error) => console.log(error))
+
+};
+
+
+
+const displayVideos = (video) => {
+    const videoContainer = document.getElementById('videos')
+    video.forEach(element => {
+        console.log(element)
+        const card = document.createElement('div')
+        card.classList = 'card card-compact'
+        card.innerHTML = `
+        <div class="">
+                  <div><figure class="h-[200px]">
+                  <img class="w-full h-full object-cover"
+                  src=${element.thumbnail}
+                  </figure></div>
+                  <div class="flex gap-5   items-center">
+                      <div class>
+                              <img class=" object-cover mt-2 h-10 w-10 rounded-full" src=${element.authors[0].profile_picture}/>
+                      </div>
+         
+                      <div class="mt-2">
+                             <h1 class="font-bold text-xl">${element.
+                             title
+                             }</h1>
+         
+                             <div class="flex gap-3 items-center">
+                             <p>${element.authors[0].profile_name}</p>
+                             <img class="h-5 w-5 " src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png" />
+                             </div>
+         
+                             <p >${element.others.views } Views</p>
+                      
+                      </div>
+                  </div>
+        </div>
     
-}
-const displayVideos = (video)=>{
-   const videoContainer = document.getElementById('videos')
-   video.forEach(element => {
-      console.log(element)
-      const card = document.createElement('div')
-      card.classList='card card-compact'
-      card.innerHTML=`
-      <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-  <div class="card-body">
-    <h2 class="card-title">
-      Card Title
-      <div class="badge badge-secondary">NEW</div>
-    </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div class="card-actions justify-end">
-      <div class="badge badge-outline">Fashion</div>
-      <div class="badge badge-outline">Products</div>
-    </div>
-  </div>`
-  videoContainer.append(card)
-   });
+
+
+        `
+
+        videoContainer.append(card)
+    });
 }
 
 LoadVideos()
